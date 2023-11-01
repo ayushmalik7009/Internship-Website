96785d777e09f57e64a2d0acccd8d036cdfb1589
Ayush Malik <120892660+ayushmalik7009@users.noreply.github.com>
2023-11-02 02:36:09 +0530

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { Home } from './pages/Home.jsx'
+import { Contact } from './pages/Contact.jsx'
 
+const router = createBrowserRouter([
+  {
+    path:"/Internship-Website/",
+    element: <App/>,
+    children:[
+      {
+        path:"/Internship-Website/",
+        element:<Home/>
+      },
+      {
+        path:"/Internship-Website/Contact",
+        element:<Contact/>
+      },
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
